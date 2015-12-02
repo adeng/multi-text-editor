93760b91945ee245db70c7537d5f38caf9f44fd2
Albert Deng <adeng@media.ucla.edu>
2015-12-02 14:56:30 -0800

    Using different null char

--- FinalProject/src/helpers/NetworkHandler.java
+++ FinalProject/src/helpers/NetworkHandler.java
@@ -83,12 +83,12 @@ public class NetworkHandler {
     }
     
     public Packet nextPacket() {
-        Scanner scan = new Scanner(isr).useDelimiter("\\0");
+        Scanner scan = new Scanner(isr).useDelimiter("\\u0000");
         return new Packet(scan.next());
     }
     
     public boolean hasNext() {
-        return new Scanner(isr).useDelimiter("\\0").hasNext();
+        return new Scanner(isr).useDelimiter("\\u0000").hasNext();
     }
 
     public BufferedReader getReader() {
